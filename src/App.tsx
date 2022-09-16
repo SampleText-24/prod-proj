32f1c3de39e3af8d3622bb02805e0e33635c8ff4
Alexey Sokolov <sample24text@gmail.com>
2022-09-16 18:12:39 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React, {Suspense, useContext, useState} from 'react';
+import './styles/index.scss'
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classnames/classNames";
+
+const App = () => {
+
+    const { theme, toggleTheme } = useTheme()
+
+    const bool = true
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <button onClick={toggleTheme}>Поменять тему</button>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О сайте</Link>
+            <Suspense fallback={<div>Loading...</div>} >
+                <Routes>
+                        <Route path={'/about'} element={<AboutPageAsync />} />
+                        <Route path={'/'} element={<MainPageAsync />} />
+
+                </Routes>
+            </Suspense>
+        </div>
+    );
+};
+
+export default App;
