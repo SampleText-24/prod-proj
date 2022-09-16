96167e787377a4df1e218762bebc529d96342c17
Alexey Sokolov <sample24text@gmail.com>
2022-09-16 19:20:36 +0700

    add Feature-Sliced Design methodology

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,12 @@
 import webpack from "webpack";
+import { BuildOptions } from "./types/config";
 
-export function buildResolvers(): webpack.ResolveOptions {
+export function buildResolvers(options: BuildOptions): webpack.ResolveOptions {
     return {
         extensions: ['.tsx', '.ts', '.js'],
+        // Позволяет не добавлять aliasы и спокойно пользоваться абсолютными импортами
+        preferAbsolute: true,
+        modules: [options.paths.src, 'node_modules'],
+        mainFiles: ['index']
     }
 }
