a0fe9357a93378723a0da5a2a2eed465c21a7b40
Alexey Sokolov <sample24text@gmail.com>
2022-09-20 20:59:18 +0700

    Add Button(UI kit), svg + pictures loaders and ThemeSwitcher

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -7,6 +7,11 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     // Выносим отдельные лоадеры т.к. важно в каком порядке они возвращаются.
     // С помощью вынесения проще увидеть чёткую последовательность
 
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -35,9 +40,20 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
             test: /\.tsx?$/,
             use: 'ts-loader',
             exclude: /node_modules/,
-        }
+    }
+
+    const fileLoader = {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+                {
+                    loader: 'file-loader',
+                },
+            ],
+    }
 
     return [
+        fileLoader,
+        svgLoader,
         typescriptLoader,
         cssLoader,
     ]
