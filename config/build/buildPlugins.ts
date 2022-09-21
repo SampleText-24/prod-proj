1776901a3481dec876563c7dd69b6d5dc99868b1
Alexey Sokolov <sample24text@gmail.com>
2022-09-21 18:37:04 +0700

    Add i18n(translate plugin), DefinePlugin(webpack config buildPlugins)

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -3,7 +3,7 @@ import webpack from "webpack";
 import {BuildOptions} from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 
-export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPluginInstance[] {
 
     // Порядок добавления плагинов особого значения не имеет в отличие от лоудеров
     return [
@@ -15,5 +15,9 @@ export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstan
             filename: 'css/[name].[contenthash:8].css',
             chunkFilename: 'css/[name].[contenthash:8].css',
         }),
+        // Плагин для создания глобальных переменных для самого приложения
+        new webpack.DefinePlugin({
+            __IS_DEV__: JSON.stringify(isDev),
+        })
     ]
 }
