a6dbb7c63dbf296f5de00827f7cc13dc912635b6
Alexey Sokolov <sample24text@gmail.com>
2023-01-13 18:18:41 +0700

    Add browser list config, Refactor fix drawer

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -59,11 +59,9 @@ export const NotificationButton = memo((props: NotificationButtonProps) => {
                 : (
                     <>
                         {trigger}
-                        <AnimationProvider>
-                            <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-                                <NotificationList />
-                            </Drawer>
-                        </AnimationProvider>
+                        <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+                            <NotificationList />
+                        </Drawer>
                     </>
                 )}
         </div>
